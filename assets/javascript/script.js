f61ff398ae7daf9587e14028d758f3de36cc21b3
Daniel Betz <daniel.betz11@gmx.de>
2014-06-15 10:15:14 +0200

    no message

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,8 @@
-//js
+$(function() {
+    //Code
+    function appendTitles() {
+		for (var i=1; i<=4; i++) {
+  			$('#data-viz').append('<img class="busses" src='images/bus.png'>');
+ 		}
+	}
+});
