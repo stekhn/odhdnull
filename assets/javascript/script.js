d28fb6488223379e99e859070fb16322ed22cee7
Daniel Betz <daniel.betz11@gmx.de>
2014-06-15 11:04:09 +0200

    no message

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,11 @@
- $('#button').click(function(){
-    for (i = 0; i < 1000; i++) {
-        //var $new = $("#first").clone().appendTo("#data").hide();
-        //$new.show();
-        $("#first").clone().insertAfter('#first');
-    }
-    $("body").animate({ scrollTop: $(document).height() }, 1000);
+$( document ).ready(function(){
+	 $('#button').click(function(){
+	    for (i = 0; i < 1000; i++) {
+	        //var $new = $("#first").clone().appendTo("#data").hide();
+	        //$new.show();
+	        $("#first").clone().insertAfter('#first');
+	    }
+	    $("body").animate({ scrollTop: $(document).height() }, 1000);
+	});
 });
 
