def533237c9a7244ac60f294f1abe388ea78896b
Walter Renner <wltr@posteo.is>
2014-06-15 12:13:09 +0200

    no message

--- assets/javascript/script_walter.js
+++ assets/javascript/script_walter.js
@@ -1,22 +1,25 @@
 $( document ).ready(function(){
 	 $('#button_bus').click(function(){
-	    for (i = 0; i < 1000; i++) {
+	    for (i = 0; i < 50; i++) {
 	        //var $new = $("#first").clone().appendTo("#data").hide();
 	        //$new.show();
 	        $("#first_bus").clone().insertAfter('#first_bus');
 	    }
 	    $("html, body").animate({ scrollTop: $(document).height() }, 1000);
-	   	$('group_nurse').show();
+	   	$('#group_nurse').show('slow');
 	});
 
 	 $('#button_nurse').click(function(){
-	    for (i = 0; i < 1000; i++) {
+	    for (i = 0; i < 100; i++) {
 	        //var $new = $("#first").clone().appendTo("#data").hide();
 	        //$new.show();
-	        $("#first").clone().insertAfter('#first');
+	        $("#first_nurse").clone().insertAfter('#first_nurse');
 	    }
 	    $("html, body").animate({ scrollTop: $(document).height() }, 1000);
-	    $('group_nurse').show();
+	    $('#group_last').show('slow');
 	});
 
+	$('#scroll_top_button').click(function(){
+		$( "html, body" ).scrollTop(0);
+	});
 });
