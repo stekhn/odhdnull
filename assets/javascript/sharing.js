af3fa654789deb83236be2843de76956d9bd31d2
Steffen Kühne <stekhn@gmail.com>
2014-06-15 13:44:37 +0200

    Fix popup handler

--- assets/javascript/sharing.js
+++ assets/javascript/sharing.js
@@ -1,57 +1,64 @@
 
-(function sharing() {
+$(document).ready(function(){
+	sharing.init();
+});
 
-	var config = {
+
+var sharing = {
+
+	config: {
 		height: 500,
 		width: 500,
-		url: "http://www.sz.de/tiananmen",
-		text: "Digitalreportage zum Jahrestag des Tiananmen-Massakers:",
-		hashtags: "tiananmen",
-		author: "von @SZ",
+		url: "https://github.com/walterrenner/odhdnull/tree/gh-pages",
+		text: "Brazillion - Alternatively investing the world cup billions: ",
+		hashtags: "worldcup",
+		author: "@opendatahackday",
 		link: "a.share",
 		textPlaceholder: "%TEXT%",
 		hashtagsPlaceholder: "%HASHTAGS%",
 		urlPlaceholder: "%URL%"
-	};
+	},
 
-	function init() {
+	init: function() {
 
 		// Select all sharing buttons with the class '.tracking'
 		var links = $(".share");
+		console.log(links);
 
 
 		// Iteratively replace URLs and bind events
 		for (var a = 0; a < links.length; a++) {
-			links[a].onclick = openPopup;
+			sharing.replaceUrl(links[a]);
+			links[a].onclick = sharing.openPopup;
 		}
-	}
+	},
 
 	// Replace the placeholder in the sharing link with the URL to be shared
-	function replaceUrl(el) {
-        var url = this.config.url;
+	replaceUrl: function(el) {
+        var url = sharing.config.url;
 		var oldUrl = el.href;
 		var newUrl = oldUrl
-			.replace(this.config.urlPlaceholder, url)
-			.replace(this.config.textPlaceholder,
-				this.config.text.split(' ').join('+') + ' ' + url + ' ' + this.config.author)
-			.replace(this.config.hashtagsPlaceholder, this.config.hashtags);
+			.replace(sharing.config.urlPlaceholder, url)
+			.replace(sharing.config.textPlaceholder,
+				sharing.config.text.split(' ').join('+') + ' ' + url + ' ' + sharing.config.author)
+			.replace(sharing.config.hashtagsPlaceholder, sharing.config.hashtags);
 		el.href = encodeURI(newUrl);
-	}
+	},
 
 	// Opens the sharing link in a new popup
-	function openPopup(e) {
+	openPopup: function(e) {
 
 		// Ensure compatibility with older browsers
 		e = (e ? e : window.event);
 		var t = (e.target ? e.target : e.srcElement);
 
 		// Set popup position, if screenwith is not avaiable use preset value
-		var px = Math.floor(((screen.availWidth || 1024) - this.config.width) / 2),
-			py = Math.floor(((screen.availHeight || 700) - this.config.height) / 2);
+		var px = Math.floor(((screen.availWidth || 1024) - sharing.config.width) / 2),
+			py = Math.floor(((screen.availHeight || 700) - sharing.config.height) / 2);
 
 		// Open popup
-		var popup = window.open(t.href, "social", "width=" + this.config.width +
-			",height=" + this.config.height + ",left=" + px + ",top=" + py +
+		var popup = window.open(t.href, "social", "width=" + sharing.config.width +
+			",height=" + sharing.config.height + ",left=" + px + ",top=" + py +
 			",location=0,menubar=0,toolbar=0,status=0,scrollbars=1,resizable=1");
 
 		if (popup) {
@@ -67,9 +74,7 @@
 
 			e.returnValue = false;
 		}
-
 		return !!popup;
 	}
-
-}());
+};
 
