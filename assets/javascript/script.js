7c7f5163daf3d857dbb203ad7cb77cad5818c1d2
Walter Renner <wltr@posteo.is>
2014-06-15 16:03:17 +0200

    no message

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,10 +1,16 @@
 $( document ).ready(function(){
-  // cost for wc and per item
-  var total_wc_cost = 11500000000;
-  var per_bus = 1000000;
-  var per_nurse = 10000000;
+  /**
+   * Cost for WorldCup and per Item
+   */
 
-  
+  var total_wc_cost = 11500000000;
+  var per_nurse = 15852;
+  var per_administration_assistant = 8400;
+  var per_busdriver = 6636;
+  var per_social = 576;
+  var per_child = 624;
+
+
   /**
    * Counter
    */
@@ -23,14 +29,17 @@ $( document ).ready(function(){
    * Button Handle
    */
 
-
-  $('#button_bus').click(function(){
-    var units = dublicateItems(per_bus, 'first_bus');
-    $("html, body").animate({ scrollTop: $(document).height() }, 1000);
-    $('#group_bus_result').text(units);
-    $('#group_bus_addition').show('slow');
-    $('#group_nurse').show('slow');
-  });
+  function dublicateItems(item_cost, itemId) {
+    var max_itters = 1000;
+    var itters = Math.ceil( total_wc_cost / item_cost );
+    if (itters < max_itters) {
+      max_itters = itters;
+    }
+    for (i = 0; i < max_itters; i++) {
+      $('#' + itemId).clone().insertAfter('#' + itemId);
+    }
+    return itters;
+  }
 
 
   $('#button_nurse').click(function(){
@@ -38,6 +47,43 @@ $( document ).ready(function(){
     $("html, body").animate({ scrollTop: $(document).height() }, 1000);
     $('#group_nurse_result').text(units);
     $('#group_nurse_addition').show('slow');
+    $('#group_administration_assistant').show('slow');
+  });
+
+
+  $('#button_administration_assistant').click(function(){
+    var units = dublicateItems(per_administration_assistant, 'first_administration_assistant');
+    $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+    $('#group_bus_result').text(units);
+    $('#group_bus_addition').show('slow');
+    $('#group_bus').show('slow');
+  });
+
+
+  $('#button_bus').click(function(){
+    var units = dublicateItems(per_busdriver, 'first_bus');
+    $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+    $('#group_bus_result').text(units);
+    $('#group_bus_addition').show('slow');
+    $('#group_nurse').show('slow');
+  });
+
+
+  $('#button_social').click(function(){
+    var units = dublicateItems(per_social, 'first_social');
+    $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+    $('#group_social_result').text(units);
+    $('#group_social_addition').show('slow');
+    $('#group_child').show('slow');
+  });
+
+
+  $('#button_child').click(function(){
+    var units = dublicateItems(per_child, 'first_child');
+    $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+    $('#group_social_result').text(units);
+    $('#group_social_addition').show('slow');
+    $('#group_user_iput').show('slow');
   });
 
 
@@ -53,23 +99,13 @@ $( document ).ready(function(){
     $('#result_user_item_name').text(name);
     $('#result_user_item_value').text(itters);
     $('#group_user_addition').show('slow');
+    $('#group_last').show('slow');
 
   });
+
 
   $('#scroll_top_button').click(function(){
     $( "html, body" ).scrollTop(0);
   });
 
-
-  function dublicateItems(item_cost, itemId) {
-    var max_itters = 1000;
-    var itters = Math.ceil( total_wc_cost / item_cost );
-    if (itters < max_itters) {
-      max_itters = itters;
-    }
-    for (i = 0; i < max_itters; i++) {
-      $('#' + itemId).clone().insertAfter('#' + itemId);
-    }
-    return itters;
-  }
 });
