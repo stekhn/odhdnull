6fdaf1f17b84994c5ee615bae9845dd2f1b9ca1d
Walter Renner <wltr@posteo.is>
2014-06-15 13:47:38 +0200

    no message

--- assets/javascript/script_walter.js
+++ assets/javascript/script_walter.js
@@ -1,43 +1,46 @@
 $( document ).ready(function(){
-   $('#button_bus').click(function(){
-      for (i = 0; i < 50; i++) {
-          //var $new = $("#first").clone().appendTo("#data").hide();
-          //$new.show();
-          $("#first_bus").clone().insertAfter('#first_bus');
-      }
-      $("html, body").animate({ scrollTop: $(document).height() }, 1000);
-      $('#group_nurse').show('slow');
+  // cost for wc and per item
+  var total_wc_cost = 14216790000;
+  var per_bus = 100000000;
+  var per_nurse = 1000000;
+
+  $('#button_bus').click(function(){
+    dublicateItems(per_bus, 'first_bus');
+
+    $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+    $('#group_nurse').show('slow');
   });
 
-   $('#button_nurse').click(function(){
-      for (i = 0; i < 100; i++) {
-          //var $new = $("#first").clone().appendTo("#data").hide();
-          //$new.show();
-          $("#first_nurse").clone().insertAfter('#first_nurse');
-      }
-      $("html, body").animate({ scrollTop: $(document).height() }, 1000);
-      $('#group_last').show('slow');
+  $('#button_nurse').click(function(){
+    for (i = 0; i < 100; i++) {
+      $("#first_nurse").clone().insertAfter('#first_nurse');
+    }
+    $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+    $('#group_last').show('slow');
   });
 
-
-   $('#button_user_input').click(function(){
+  $('#button_user_input').click(function(){
     var val = $( "#usersvalue" ).val();
-    if (val === "" || val<1000) {
-          alert("please enter a number greater than 1000");
+    if (val === "" || val < 1000) {
+      alert("please enter a number greater than 1000");
     } else {
       for (i = 0; i < val; i++) {
-        var $new = $("#first").clone().appendTo("#data").hide();
-        $new.show();
         $("#first_nurse").clone().insertAfter('#first_nurse');
       }
       $("html, body").animate({ scrollTop: $(document).height() }, 1000);
       $('#group_last').show('slow');
-
-
     }
   });
 
   $('#scroll_top_button').click(function(){
     $( "html, body" ).scrollTop(0);
   });
+
+
+  function dublicateItems (item_cost, itemId) {
+    var itters = Math.ceil( total_wc_cost / item_cost );
+    for (i = 0; i < itters; i++) {
+      $('#' + itemId).clone().insertAfter('#' + itemId);
+    }
+  }
 });
