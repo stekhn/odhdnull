f2c5b2a96c1d5d6c78ce978968d702ea4602828f
Daniel Betz <daniel.betz11@gmx.de>
2014-06-15 11:53:04 +0200

    no message

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,24 @@
 $( document ).ready(function(){
-	 $('#button').click(function(){
+	
+	/**
+	 * Counter
+	 */
+
+	var options = {
+	  useEasing : true, 
+	  useGrouping : true, 
+	  separator : '.', 
+	  decimal : '.' 
+	};
+
+	var demo = new countUp("count", 0, 10800000000, 0, 2.5, options);
+	demo.start();
+
+	/**
+	 * Button Handle
+	 */
+
+	$('#button').click(function(){
 	    for (i = 0; i < 1000; i++) {
 	        //var $new = $("#first").clone().appendTo("#data").hide();
 	        //$new.show();
