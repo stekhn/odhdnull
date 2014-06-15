200b2ece81095cd38894f128667087f6acaf4e06
Daniel Betz <daniel.betz11@gmx.de>
2014-06-15 10:26:31 +0200

    no message

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,8 @@
 $(function() {
     //Code
-    function appendTitles() {
-		for (var i=1; i<=4; i++) {
-  			$('#data-viz').append('<img class="busses" src='images/bus.png'>');
- 		}
-	}
+ //    function appendTitles() {
+	// 	for (var i=1; i<=4; i++) {
+ //  			$('#data-viz').append('<img class="busses" src='images/bus.png'>');
+ // 		}
+	// }
 });
